629ef1f58f03d7b8bc7d35cf47b8a4dfa5accd40
yos244 <yos_24@hotmail.com>
2023-01-03 20:52:31 +0000

    Changed the setAreas  to the correct format

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,23 @@
-import { useEffect, useState } from 'react'
-import { getAreaData } from './api'
+import { useEffect, useState } from "react";
+import { getAreaData } from "./api";
 
-import './App.css'
+import "./App.css";
 
 function App() {
-
   const [areas, setAreas] = useState([]);
 
   const load = async () => {
     try {
-      const areaData = await getAreaData()
-
+      const areaData = await getAreaData();
       areas.concat(areaData);
-  
-      setAreas(areas);
+
+      setAreas((currArr) => {
+        return areaData;
+      });
     } catch (error) {
-      window.alert("todo: fix app")
+      window.alert("todo: fix app");
     }
-  }
+  };
 
   useEffect(() => {
     load();
@@ -28,7 +28,7 @@ function App() {
       <h1>Postcoders</h1>
       <h2>{`Areas for BB10: ${areas.length}`}</h2>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
